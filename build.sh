66c4271f8116c9acf3728d8cbccf1969eeb9b290
subbasri17 <subasundaram17@gmail.com>
2026-03-09 15:19:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,30 +9,26 @@ DEV_IMAGE="$DOCKER_USER/dev:$BRANCH"
 PROD_IMAGE="$DOCKER_USER/prod:$BRANCH"
 
 echo "Building Docker image..."
-docker build -t "$IMAGE_NAME:latest" .
+docker build -t $IMAGE_NAME:latest .
 
 # Check if Docker credentials exist
-if [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
-    echo "Error: DOCKER_USER or DOCKER_PASS is not set!"
-    exit 1
-fi
+#if [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
+#   echo "Error: DOCKER_USER or DOCKER_PASS is not set!"
+#    exit 1
+#i
 
-echo "Logging in to DockerHub..."
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+#echo "Logging in to DockerHub..."
+#echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
 if [ "$BRANCH" == "dev" ]; then
-    IMAGE="$DEV_IMAGE"
-elif [ "$BRANCH" == "master" ]; then
-    IMAGE="$PROD_IMAGE"
+    echo "Tagging image for dev repo..."
+    docker tag $IMAGE_NAME:latest $DEV_IMAGE
+elif [ "$BRANCH" == "main" ]; then
+     echo "Tagging image for prod repo..."
+     docker tag $IMAGE_NAME:latest $PROD_IMAGE
 else
     echo "Unknown branch '$BRANCH'. Only dev or master allowed."
     exit 1
 fi
 
-echo "Tagging image $IMAGE_NAME:latest -> $IMAGE"
-docker tag "$IMAGE_NAME:latest" "$IMAGE"
-
-echo "Pushing image to DockerHub..."
-docker push "$IMAGE"
-
-echo "Build and push complete!"
+echo "Build complete."
